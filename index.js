ef0a469caf689b23fa23a280c73e79116397f938
Emilio Martinez Rivero <emiliomartinezriv@gmail.com>
2020-11-13 10:00:04 +0100

    commit

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ import { map } from 'mapea-prueba/mapea';
 
 const mapjs = map({
   container: 'map',
+  bbox: [225341.2751410681, 4145371.596340267, 245014.72445818176, 4145371.596340267]
 });
+
+window.mapjs = mapjs;
